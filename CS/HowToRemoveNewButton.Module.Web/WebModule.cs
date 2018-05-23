372fc5083907681a43777c837fec2abef12076a0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToRemoveNewButton.Module.Web/WebModule.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace HowToRemoveNewButton.Module.Web {
+   [ToolboxItemFilter("Xaf.Platform.Web")]
+   public sealed partial class HowToRemoveNewButtonAspNetModule : ModuleBase {
+      public HowToRemoveNewButtonAspNetModule() {
+         InitializeComponent();
+      }
+   }
+}
